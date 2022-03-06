3090e148806196989689e08a62b3e5c2c2eaaafa
Zia <54710635+globalpolaris@users.noreply.github.com>
2022-03-06 11:41:36 +0700

    show words based on alphabet

--- src/components/words.js
+++ src/components/words.js
@@ -6,12 +6,65 @@ const obj = require("../word.json");
 export default function Words() {
   const [wordList, setWordList] = React.useState(obj.words);
   const [toDisplay, setToDisplay] = React.useState(500);
+  const [activeIndex, setActiveIndex] = React.useState(-1);
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  const displayWordStartsWith = (letter) => {
+    let newArr = obj.words.filter((word) => word.startsWith(letter));
+    setWordList(newArr);
+  };
+
   return (
-    <div className="grid grid-cols-3 justify-items-center">
-      <h1 className="col-span-3 font-bold text-lg my-5">Kata-Kata</h1>
-      {wordList.slice(0, toDisplay).map((word) => {
-        return <p key={word}>{word}</p>;
-      })}
+    <div>
+      <h1 className="text-center font-bold text-lg my-5">Kata-Kata</h1>
+      <div className="flex flex-row justify-center my-3 flex-wrap">
+        {alphabet.map(function (alphabet, i) {
+          return (
+            <button
+              onClick={() => {
+                displayWordStartsWith(alphabet);
+                setActiveIndex(i);
+              }}
+              className={i === activeIndex ? "text-red-800 font-bold m-2 underline underline-offset-1" : "m-2"}
+              key={alphabet}
+            >
+              {alphabet}
+            </button>
+          );
+        })}
+      </div>
+      <div className="grid grid-cols-3 justify-items-center my-5">
+        {wordList.slice(0, toDisplay).map((word) => {
+          return <p key={word}>{word}</p>;
+        })}
+      </div>
     </div>
   );
 }
